f4bb0116b081ec40489750e985d5064acb775441
Magesh Kuppan <tkmagesh77@gmail.com>
2020-09-17 12:21:46 +0530

    calculator demo

--- 03-react-native/first-app/App.js
+++ 03-react-native/first-app/App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import StopWatch from './StopWatch';
+import Calculator from './Calculator';
 
 export default function App(){
   return(
     <View style={styles.container}>
-      <StopWatch/>
+      <Calculator/>
     </View>
   )
 }
