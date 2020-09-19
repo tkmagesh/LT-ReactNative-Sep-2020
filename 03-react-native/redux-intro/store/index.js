ec35e8f8314ea5d9ea592d070503f0a23b77be65
Magesh Kuppan <tkmagesh77@gmail.com>
2020-09-19 11:54:24 +0530

    logger middleware implementation

--- 03-react-native/redux-intro/store/index.js
+++ 03-react-native/redux-intro/store/index.js
@@ -1,10 +1,26 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 //import spinnerReducer from '../reducers/spinnerReducer';
 import rootReducer from '../reducers';
 
 
-//const appStore = createStore(spinnerReducer);
-const appStore = createStore(rootReducer);
+/* 
+function loggerMiddleware(store){
+    return function(next){
+        return function(action){
+            console.log(action.type);
+            next(action);
+        }
+    }
+} 
+*/
 
-console.log(appStore.getState());
+const loggerMiddleware = (store) => (next) => (action) => {
+    console.log(action.type);
+    next(action);
+}
+
+//const appStore = createStore(spinnerReducer);
+const appStore = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+
+
 export default appStore;
